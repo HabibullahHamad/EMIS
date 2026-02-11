90ba08a6c0f14ee68474c2dc5ef7a5d14fd251b4
HabibullahHamad <habibullahtotakhel2020@gmail.com>
2026-02-11 18:28:03 +0430

    LAST NIGHT

--- resources/views/Administrations/create.blade.php
+++ resources/views/Administrations/create.blade.php
@@ -1,11 +1,14 @@
 @extends('new')
-
 @section('content')
+
+tarnslate all lables 
+
+
 <div class="container">
     <h3>Add New User</h3>
     <form method="POST" action="{{ route('users.store') }}">
         @csrf
-        <div class="mb-3"><label>Name</label><input name="name" class="form-control" required></div>
+        <div class="mb-3"><label>Name </label><input name="name" class="form-control" required></div>
         <div class="mb-3"><label>Email</label><input name="email" class="form-control" required></div>
         <div class="mb-3"><label>Username</label><input name="username" class="form-control" required></div>
         <div class="mb-3"><label>Role</label>
@@ -13,6 +16,7 @@
                 <option value="admin">Admin</option>
                 <option value="user">User</option>
             </select>
+            
         </div>
         <div class="mb-3"><label>Password</label><input name="password" type="password" class="form-control" required></div>
         <button class="btn btn-success">Save</button>
